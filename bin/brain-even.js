27bc164a05ae9ee7b8916539d186c010cc025002
ukazova ay <anastasia943@mail.ru>
2021-03-22 19:44:40 +0300

    step 5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,42 +1,38 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-//import welcome from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-//const welcome = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-//  };
-//welcome();
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no"');
 let value = true;
 let time = 0;
 let correctAnswer = '';
 
 while (value) {
-    const functionRandom = () => {
-        const min = 0;
-        const max = 100;
-        return Math.floor(Math.random() * (max - min)) + min;
+  const functionRandom = () => {
+    const min = 0;
+    const max = 100;
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+  const randomNumber = functionRandom();
+  console.log('Question:', randomNumber);
+  const answer = readlineSync.question('Your answer: ');
+  if (randomNumber % 2 === 0) {
+    correctAnswer = 'yes';
+  } else {
+    correctAnswer = 'no';
+  }
+  if ((randomNumber % 2 === 0 && answer === 'yes') || (randomNumber % 2 !== 0 && answer === 'no')) {
+    value = true;
+    time += 1;
+    if (time === 3) {
+      console.log(`Congradulation, ${name}!`);
+      value = false;
     }
-    const randomNumber = functionRandom ();
-    console.log('Question:', randomNumber);
-    const answer = readlineSync.question('Your answer: ');
-    if (randomNumber % 2 === 0) {
-        correctAnswer = 'yes';
-    } else {
-        correctAnswer = 'no'
-    }
-    if ((randomNumber % 2 === 0 && answer === 'yes') || (randomNumber % 2 !== 0 && answer === 'no')) {
-        value = true;
-        time = time + 1;
-        if (time === 3) {
-            console.log(`Congradulation, ${name}!`);
-            value = false;
-        }
-    } else {
-        console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}"`);
-        console.log(`Let's try again, ${name}!`);
-        value = false;
-    }
+  } else {
+    console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}"`);
+    console.log(`Let's try again, ${name}!`);
+    value = false;
+  }
 }
